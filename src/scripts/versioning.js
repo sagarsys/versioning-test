0873fd3daccb4945a709d4f1c84894e0fbfa5bbb
ssawuck <ssawuck@maureva.com>
2019-08-29 17:01:35 +0400

    -- [WIP] Versioning script

--- src/scripts/versioning.js
+++ src/scripts/versioning.js
@@ -1,3 +1,32 @@
 /**
  * Script to version files
  */
+const fs = require('fs');
+const path = require('path');
+const { environment } = require('../../environment.js');
+
+const sonarProject = './sonar-project.properties';
+const env = './environment.js';
+const packageJson = './package.json';
+
+
+const getCurrentVersion = () => {
+    const packageContents = fs.readFileSync(path.resolve(packageJson));
+    let content;
+    try {
+        content = JSON.parse(packageContents);
+    } catch (e) {
+        console.log(e);
+    }
+    return content && content.version;
+};
+
+const version = getCurrentVersion();
+const sonarProps = fs.readFileSync(path.resolve(env), 'utf-8');
+console.log(environment);
+environment.version = version;
+console.log(environment);
+
+console.log(sonarProps);
+
+
