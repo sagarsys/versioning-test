19bddeac373aab47a5064fa85ce7c8e263a73e2e
sagar <sagar_sys@live.com>
2019-08-29 23:12:07 +0400

    Use FS to update versions in required files

--- environment.js
+++ environment.js
@@ -1,9 +1,8 @@
 const environment = {
-  version: '0.1.0',
-  production: false,
+    version: '0.1.2',
+    production: false,
 };
 
 module.exports = {
-  environment
+    environment
 };
-
--- src/scripts/versioning.js
+++ src/scripts/versioning.js
@@ -3,30 +3,70 @@
  */
 const fs = require('fs');
 const path = require('path');
-const { environment } = require('../../environment.js');
+const { EOL } = require('os');
 
-const sonarProject = './sonar-project.properties';
-const env = './environment.js';
-const packageJson = './package.json';
-
+// file paths
+const sonarProjectPath = './sonar-project.properties';
+const envPath = './environment.js';
+const packageJsonPath = './package.json';
+// strings to match in files
+const sonarVersion = 'sonar.projectVersion';
+const envVersion = 'version:';
 
 const getCurrentVersion = () => {
-    const packageContents = fs.readFileSync(path.resolve(packageJson));
     let content;
     try {
+        const packageContents = fs.readFileSync(path.resolve(packageJsonPath), 'utf-8');
         content = JSON.parse(packageContents);
-    } catch (e) {
-        console.log(e);
+    } catch ( e ) {
+        console.error('Failed to read version from package.json', e);
     }
     return content && content.version;
 };
 
 const version = getCurrentVersion();
-const sonarProps = fs.readFileSync(path.resolve(env), 'utf-8');
-console.log(environment);
-environment.version = version;
-console.log(environment);
 
-console.log(sonarProps);
+const updateSonarPropertiesFile = () => {
+    const sonarProps = fs.readFileSync(path.resolve(sonarProjectPath), 'utf-8');
+    const lines = sonarProps.split(EOL);
+    const regex = new RegExp(sonarVersion, 'g');
+    const updatedLines = lines.map(line => {
+        if ( regex.test(line) ) {
+            const end = line.indexOf('=') + 1;
+            return line.substring(0, end) + version;
+        }
+        return line;
+    });
+    const updatedSonarProps = updatedLines.join(EOL);
+    try {
+        fs.writeFileSync(sonarProjectPath, updatedSonarProps);
+        console.info(`Updated version successfully in "${sonarProjectPath}"`);
+    } catch ( e ) {
+        console.error(`Failed to update version in "${sonarProjectPath}"`, e);
+    }
+};
 
+updateSonarPropertiesFile();
 
+const updateEnvFile = () => {
+    const env = fs.readFileSync(path.resolve(envPath), 'utf-8');
+    const lines = env.split(EOL);
+    const regex = new RegExp(envVersion, 'gi');
+    const updatedLines = lines.map(line => {
+        if ( regex.test(line) ) {
+            const end = line.indexOf(':');
+            return `${line.substring(0, end)}: '${version}',`;
+        }
+        return line;
+    });
+    const updatedEnv = updatedLines.join(EOL);
+    try {
+        fs.writeFileSync(envPath, updatedEnv);
+        console.info(`Updated version successfully in ${envPath} file`);
+    } catch ( e ) {
+        console.error(`Failed to update version in "${envPath}"`, e);
+    }
+
+};
+
+updateEnvFile();
